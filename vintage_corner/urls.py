f3c3f4da523a3aabfbb58cbf97b58ec13cd1a1fb
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-10-29 15:11:54 +0000

    start adjusting margins on product detail page

--- vintage_corner/urls.py
+++ vintage_corner/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('shopping_cart/', include('shopping_cart.urls')),
 ]
