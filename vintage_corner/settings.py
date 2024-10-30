7ca89cf89b30236fc6f40fa30421fa3038af4691
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-10-30 13:14:59 +0000

    create shopping cart contexts and logic to calculate delivery costs

--- vintage_corner/settings.py
+++ vintage_corner/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shopping_cart.contexts.cart_contents',
             ],
         },
     },
@@ -163,3 +164,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 10
