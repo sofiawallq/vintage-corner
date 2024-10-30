1d869f65da26acab4c7d6fb87aace870fd90df23
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-10-30 12:28:47 +0000

    add all product images to media folder and create path for them

--- vintage_corner/urls.py
+++ vintage_corner/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('shopping_cart/', include('shopping_cart.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
