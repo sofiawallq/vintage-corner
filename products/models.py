bec13ab4465bfcc216c75ae3e7f5789a7a6ddee5
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-10-30 13:05:29 +0000

    update product model to generate random sku for each new added product

--- products/models.py
+++ products/models.py
@@ -1,4 +1,11 @@
+import random
+import string
 from django.db import models
+
+
+def generate_sku():
+    """Generates a random SKU for every new product that is added to the store"""
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
 
 
 class Category(models.Model):
@@ -23,7 +30,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    sku = models.CharField(max_length=250, null=True, blank=True)
+    sku = models.CharField(max_length=250, unique=True, default=generate_sku)
 
     def __str__(self):
         return self.name
