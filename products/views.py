e868696fc1bf466f0b78cfdfb5ca4c46d1045984
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-11-08 11:20:04 +0000

    add function for admin to delete products on site, plus add edit/delete buttons to product templates

--- products/views.py
+++ products/views.py
@@ -59,13 +59,13 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """ Add a product to the store """
+    """ Admin - Add a product to the store """
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -80,7 +80,7 @@ def add_product(request):
 
 
 def edit_product(request, product_id):
-    """ Edit a product in the store """
+    """ Admin - Edit a product in the store """
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -101,3 +101,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Admin - Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))    
