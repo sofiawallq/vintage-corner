e3ee71bb21a9d970060150b6581c52127ba38f35
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-11-09 14:27:00 +0000

    add products view to home page and fix margins on product page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from products.models import Product, Category
+from products.views import all_products
 
 # Create your views here.
 def index(request):
-    """ A view to return the home/index page """
-    
-    return render(request, 'home/index.html')
+    """ A view to return the home/index page with products """
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
+
