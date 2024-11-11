cc7f4cfe948aee62b3e983c1514b0437e527c86f
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-11-11 10:23:15 +0000

    add views for review and response, plus create templates for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product, Review, Response
+from profiles.models import UserProfile
+from .forms import ReviewForm, ResponseForm
 
-# Create your views here.
+
+@login_required
+def add_review(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, user_profile=user_profile)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('review_list')
+    else:
+        form = ReviewForm()
+    return render(request, 'add_review.html', {'form': form})
+
+
+@login_required
+def add_response(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    
+    if request.method == 'POST':
+        form = ResponseForm(request.POST)
+        if form.is_valid():
+            response = form.save(commit=False)
+            response.review = review
+            response.user = request.user
+            response.save()
+            return redirect('review_detail', review_id=review.id)
+    else:
+        form = ResponseForm()
+    return render(request, 'add_response.html', {'form': form, 'review': review})
