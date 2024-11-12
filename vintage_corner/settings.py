737620b2cf3c955f7144710de7c895e8501ed0b9
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-11-12 12:45:26 +0000

    add cryptography manually to requ irements.txt

--- vintage_corner/settings.py
+++ vintage_corner/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-sofiawallq-vintagecorne-ghzhx4etuci.ws.codeinstitute-ide.net', 'https://vintage-corner-9f26bce1e332.herokuapp.com/', 'localhost']
 
