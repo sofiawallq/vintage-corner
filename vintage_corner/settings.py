ccf63752904d2d807811c312c34cb9c436aad995
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-11-16 14:48:15 +0000

    add whitenoise to middleware

--- vintage_corner/settings.py
+++ vintage_corner/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'vintage_corner.urls'
