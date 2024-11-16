03f4eb02fd9276a405a1d418706d675c05adfa08
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-11-16 21:13:23 +0000

    update custom_storage.py due to wrong file path

--- custom_storages.py
+++ custom_storages.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
+    location = settings.MEDIAFILES_LOCATION
     file_overwrite = False
